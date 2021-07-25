03bc266754351b807f799ccab5a4ff84d28da3fc
Martín Javier Del Percio <martinjavierd@gmail.com>
2021-07-24 22:56:41 -0300

    Se agregó el código JS

--- script/app.js
+++ script/app.js
@@ -1,118 +1,31 @@
-
-// VARIABLES
-
-var nombre = "Julian Matías";
-var apellido = "Fuoco";
-
-console.log(nombre);
-
-// STRING
-
-var lista = ["Julian",'Matías',`Fuoco`];
-
-console.log(lista);
-
-// NUMBER
-
-let edad = 30;
-console.log(edad);
-
-// BOOLEAN
-
-const casaPropia = false;
-console.log(casaPropia);
-
-// OPERADORES
-
-let numero1 = 20;
-let numero2 = 10;
-
-let suma = numero1 + numero2;
-let resta = numero1 - numero2;
-let multi = numero1 * numero2;
-let divi = numero1 / numero2;
-
-console.log(suma);
-console.log(resta);
-console.log(multi);
-console.log(divi);
-
-// CONCATENAR
-
-console.log(nombre + " " + apellido);
-
-// CONVERTIR 
-
-// NULOS
-
-// UNDEFINED
-
-
-
-let numero1 = Number(prompt("Ingrese el primer número"));
-let numero2 = Number(prompt("Ingrese el segundo número"));
-
-console.log("SUMA: " + (numero1 + numero2));
-console.log("RESTA: " + (numero1 - numero2));
-console.log("MULTIPLICACIÓN: " + (numero1 * numero2));
-console.log("DIVISIÓN: " + (numero1 / numero2));
+// Solicita nombre y apellido del usuario
 
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 
-console.log("Nombre Completo: " + nombre + " " + apellido);
+// Solicita la edad del usuario
 
+let edad = Number(prompt("Ingrese su edad"));
 
-// CONDICIONALES
+// Solicita la clave para ingresar
 
-let test = "SENTENCIA DEL IF";
-let test2 = "SNETENCIA DEL ELSE";
-
-let condicion = confirm("¿Qué condición querés?");
-
-if (condicion) {
-    console.log(test);
-} else {
-    console.log(test2);
-}
-
-
-
-let nombre = prompt("Ingresa tu nombre");
-let confirmacion = confirm("¿Quieres entrar?");
-
-if (nombre == "Julian" || nombre == "julian")
-{
-    console.log("Tu nombre es Julian");
-} else {
-    console.log("Tu nombre no es Julian.");
-}
-
-if (confirmacion)
-{
-    console.log("Y Quieres Ingresar");
-} else {
-    console.log("Y No Quieres Ingresar");
-}
-
-*/
-
-// Pedir cuatro datos
-// Nombre, Contraseña, Año Nacimiento y Año Actual
-
-// Comparamos si la contraseña es "coderhouse"
-// Tengo que mostrar en Alert mi edad actual y mostrar "Julian tenes 4 años"
-// Sino, digo que la contraseña es incorrecta
-
-let nombre = prompt("¿Cuál es tu nombre?");
 let clave = prompt("Ingresa la contraseña");
 clave = clave.toLowerCase();
 
+// Verifica si la clave es correcta
+
 if (clave == "coderhouse")
 {
-    let anioNac = Number(prompt("Ingresa tu año de nacimiento"));
-    let anioActual = Number(prompt("Ingresa el año actual"));
-    alert(nombre + " tenés " + (anioActual - anioNac) + " años");
+    let verificaNombre = confirm("¿Es " + nombre + " su nombre?");
+    let verificaApellido = confirm("¿Es " + apellido + " su apellido?");
+    let verificaEdad = confirm("¿Es " + edad + " su edad actual?");
+    if (verificaNombre && verificaApellido && verificaEdad)
+    {
+        alert("Bienvenido " + nombre + " " + apellido + " puede ingresar por la puerta 5");
+    } else {
+        alert("Usted es un impostor");
+    }
 } else {
     alert("La contraseña es incorrecta");
 }
+
