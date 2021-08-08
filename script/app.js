00ab71bf35ef6b0909128b391635fd59cb4951eb
Martín Javier Del Percio <martinjavierd@gmail.com>
2021-08-08 16:57:18 -0300

    Clase0506

--- script/app.js
+++ script/app.js
@@ -1,185 +1,72 @@
-// DEFINICIÓN DE VARIABLES
+// Definición de variables
 
-var usuario = "";
-var contrasenia = "";
-var userLogged = false;
-var opcionSeleccionada = 0;
-let tries = 3;
-var ETH = 20;
-var BTC = 40;
-var SHIB = 1000;
-var SUN = 1000;
-var ADA = 2000;
-var DOT = 3000;
+// Valores en U$D
+var ETH = 2987.01;
+var BTC = 43787.01;
+var SHIB = 0.000007442;
+var SUN = 0.02397;
+var ADA = 1.43;
+var DOT = 19.56;
 
-// FUNCIONES DE LA APLICACIÓN
+// Tenencias
+var cantETH = 2.5;
+var cantBTC = 1.5;
+var cantSHIB = 30000.0;
+var cantSUN = 5000.0;
+var cantADA = 50.0;
+var cantDOT = 30.0;
 
-// Mostrar el menú en la consola
+// Arreglos
+var nombreToken = ["ETH", "BTC", "SHIB", "SUN", "ADA", "DOT"];
+var valorToken = [ETH, BTC, SHIB, SUN, ADA, DOT];
+var descToken = ["Ethereum", "Bitcoin", "Shiba Inu", "Sun", "Cardano", "Polkadot"]
+var cantToken = [cantETH, cantBTC, cantSHIB, cantSUN, cantADA, cantDOT];
 
-function muestraMenu(){
+// Acumulador
+var montoTotal = 0.0;
 
-    // Muestra las opciones
-    console.log("        ");
-    console.log("OPCIONES");
-    console.log("1. Mostrar la tabla de valores");
-    console.log("2. Editar uno de los valores");
-    console.log("3. Borrar uno de valores");
-    console.log("4. Salir del sistema");
+// Título de la Página
 
+var tituloPagina = `<h1>Tu Billetera de Criptomonedas</h1>`;
+
+// Nombre de usuario
+
+var nombreUsuario = `<h2>Usuario: gregorio2020</h2>`;
+
+// Nombre de las Columnas 
+
+var nombreColumnas = `<tr><th>#</th><th>Token</th><th>Valor</th><th>Descripción</th><th>Cantidad</th><th>Monto</th></tr>`;
+
+// Filas con la información
+
+var filasInformacion = "";
+
+// Definición de Funciones
+
+function calcularValor(valorA, valorB)
+{
+    var resultado = (valorA*valorB).toFixed(2)
+    montoTotal = montoTotal + Number(resultado);
+    return resultado;
 }
 
-function mostrarTabla(){
-
-    // Limpia la consola
-    console.clear();
-    // Muestra los valores
-    console.log(`Moneda  Valor`);
-    console.log(`ETH     ${ETH}`);
-    console.log(`BTC     ${BTC}`);
-    console.log(`SHIB    ${SHIB}`);
-    console.log(`SUN     ${SUN}`);
-    console.log(`ADA     ${ADA}`);
-    console.log(`DOT     ${DOT}`);
-
+function formateaValor(valorA)
+{
+    var resultado = Number(valorA).toLocaleString('en');
+    return resultado;
 }
 
-function editarValor(){
+// Arma la Información para la tabla
 
-    // Ingrese el nombre de la moneda
-    var nombreMoneda;
-    do{
-        nombreMoneda = prompt("Ingrese el nombre de la moneda");    
-    } while((nombreMoneda != "ETH" && nombreMoneda != "BTC" && nombreMoneda != "SHIB" && nombreMoneda != "SUN"  && nombreMoneda != "ADA" && nombreMoneda != "DOT"))
-    
-    // Ingrese el nuevo valor
-    let nuevoValor = Number(prompt("Por favor, ingrese el nuevo valor"));
-    
-    // Asigna el nuevo valor
-    switch(nombreMoneda)
-    {
-        case "ETH":
-            ETH = nuevoValor;
-            break;
-        case "BTC":
-            BTC = nuevoValor;
-            break;
-        case "SHIB":
-            SHIB = nuevoValor;
-            break;
-        case "SUN":
-            SUN = nuevoValor;
-            break;
-        case "ADA":
-            ADA = nuevoValor;
-            break;
-        case "DOT":
-            DOT = nuevoValor;
-            break;
-    }
-
+for (var index = 0; index < nombreToken.length; index++)
+{    
+    filasInformacion = filasInformacion + `<tr><td>${index}</td><td>${nombreToken[index]}</td><td>${valorToken[index]}</td><td>${descToken[index]}</td><td>${cantToken[index]}</td><td>${calcularValor(cantToken[index], valorToken[index])}</td></tr>`;
 }
 
-function borrarValor(){
+filasInformacion = filasInformacion + `<tr><td></td><td></td><td></td><td></td><td>TOTAL</td><td>${formateaValor(montoTotal)}</td></tr>`;
 
-    // Ingrese el nombre de la moneda a borrar
-    var nombreMoneda;
-    do{
-        nombreMoneda = prompt("Ingrese el nombre de la moneda");    
-    } while((nombreMoneda != "ETH" && nombreMoneda != "BTC" && nombreMoneda != "SHIB" && nombreMoneda != "SUN"  && nombreMoneda != "ADA" && nombreMoneda != "DOT"))
-    
-    // Borra el valor
-    switch(nombreMoneda)
-    {
-        case "ETH":
-            ETH = 0;
-            break;
-        case "BTC":
-            BTC = 0;
-            break;
-        case "SHIB":
-            SHIB = 0;
-            break;
-        case "SUN":
-            SUN = 0;
-            break;
-        case "ADA":
-            ADA = 0;
-            break;
-        case "DOT":
-            DOT = 0;
-            break;
-    }
+// Muestra en el navegador los datos calculados
 
-}
-
-function salirDelSistema(){
-    // Cambia valor de la variable
-    userLogged = false;
-    // Muestra mensaje
-    console.log("    ");
-    console.log("Proceso de logout exitoso");
-}
-
-function ingresarAlSistema(){
-    // Limpia la consola
-    console.clear();
-    do{
-        // Solicita usuario
-        usuario = prompt("Ingrese el nombre de usuario");
-        usuario = usuario.toString();
-        // Solicita contraseña
-        contrasenia = prompt("Ingrese la contraseña del usuario");
-        contrasenia = contrasenia.toString();
-
-        muestraValores();
-
-        // Verifica datos ingresados
-        if ((usuario == "coderhouse") && (contrasenia == "16810")){
-            userLogged = true;
-        } else {
-            tries--;
-            console.log("Datos incorrectos, intente una vez más");
-        }
-    } while (tries>0 && userLogged == false)
-
-     if (tries=0){
-        console.log("Superó la cantidad de tres intentos"); 
-     }
-}
-
-function muestraValores(){
-    console.log("Usuario: " + usuario);
-    console.log("Contrasenia: " + contrasenia);
-    console.log("Intentos: " + tries);
-    console.log("Opción: " + opcionSeleccionada);
-    console.log("userLogged: " + userLogged);
-}
-
-do{
-
-    if (userLogged){
-        muestraMenu();
-        opcionSeleccionada = Number(prompt("Ingrese una de las opciones"));
-        switch(opcionSeleccionada){
-            case 1:
-                mostrarTabla();
-                break;
-            case 2:
-                editarValor();
-                break;
-            case 3:
-                borrarValor();
-                break;
-            case 4:
-                salirDelSistema();
-                break;
-            default:
-                console.log("Esa opción no existe, pruebe nuevamente"); 
-                break;
-        }
-    } else {
-        ingresarAlSistema();
-    }
-
-} while(opcionSeleccionada != 4)
-
+document.open();
+document.write(tituloPagina + nombreUsuario + "<table>" + nombreColumnas + filasInformacion + "</table>");
+document.close();
