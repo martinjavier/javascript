40225187e6d3cc4223f22e806ed9670d59c2fceb
Martín Javier Del Percio <martinjavierd@gmail.com>
2021-08-27 11:54:15 -0300

    Mejora de estilos css

--- billetera/billetera.js
+++ billetera/billetera.js
@@ -153,7 +153,7 @@ function muestroTabla()
 
     // Nombre de las Columnas 
 
-    var nombreColumnas = `<tr><th>#</th><th>Token</th><th>Precio</th><th>Cantidad</th><th>Valores</th><th>Borrar</th><th>Editar</th></tr>`;
+    var nombreColumnas = `<tr><th>#</th><th>Token</th><th>Precio</th><th>Cantidad</th><th>Valores</th><th class="text-center">Borrar</th><th class="text-center">Editar</th></tr>`;
 
     // Filas con la información
 
@@ -180,8 +180,14 @@ function muestroTabla()
 
         precioFormateado = formateaValor(valorCalculado);   
 
-        filasInformacion = filasInformacion + `<tr><td>${i+1}</td><td>${misMonedas[i].nombre}</td><td>${precio}</td><td>${formateaValor(misMonedas[i].cantidad)}</td><td>${precioFormateado}</td><td><button id="'${misMonedas[i].nombre}'" onclick="deleteCoin('${misMonedas[i].nombre}')">X</button></td><td><button id="'${misMonedas[i].nombre}'" onclick="editCoin('${misMonedas[i].nombre}');">edit</button></td></tr>`;
-
+        filasInformacion = filasInformacion + `<tr><td>${i+1}</td>
+        <td>${misMonedas[i].nombre}</td>
+        <td>${precio}</td>
+        <td>${formateaValor(misMonedas[i].cantidad)}</td>
+        <td>${precioFormateado}</td>        
+        <td align="center"><button id="'${misMonedas[i].nombre}'" onclick="deleteCoin('${misMonedas[i].nombre}')" type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path></svg></button></td>
+        <td align="center"><button type="button" class="btn btn-lg btn-primary" id="'${misMonedas[i].nombre}'" onclick="editCoin('${misMonedas[i].nombre}');">Editar</button></td>
+        </tr>`;
     }
 
     // Obtengo el identificador del elemento tabla en el DOM
